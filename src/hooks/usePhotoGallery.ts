49d497428b802f11116aa2818b132846acbeaa64
Juho Kalberg <juss@vilenkell.eu>
2022-05-29 23:52:29 +0300

    added photos saving functionality

--- src/hooks/usePhotoGallery.ts
+++ src/hooks/usePhotoGallery.ts
@@ -11,8 +11,45 @@ import { Filesystem, Directory } from '@capacitor/filesystem';
 import { Storage } from '@capacitor/storage';
 import { Capacitor } from '@capacitor/core';
 
+const PHOTO_STORAGE = 'photos';
+
 export function usePhotoGallery() {
+  const savePicture = async (
+    photo: Photo,
+    fileName: string
+  ): Promise<UserPhoto> => {
+    const base64Data = await base64FromPath(photo.webPath!);
+    const savedFile = await Filesystem.writeFile({
+      path: fileName,
+      data: base64Data,
+      directory: Directory.Data,
+    });
+
+    // Use webPath to display the new image instead of base64 since it's
+    // already loaded into memory
+    return {
+      filepath: fileName,
+      webviewPath: photo.webPath,
+    };
+  };
   const [photos, setPhotos] = useState<UserPhoto[]>([]);
+  useEffect(() => {
+    const loadSaved = async () => {
+      const { value } = await Storage.get({ key: PHOTO_STORAGE });
+      const photosInStorage = (value ? JSON.parse(value) : []) as UserPhoto[];
+
+      for (let photo of photosInStorage) {
+        const file = await Filesystem.readFile({
+          path: photo.filepath,
+          directory: Directory.Data,
+        });
+        // Web platform only: Load the photo as base64 data
+        photo.webviewPath = `data:image/jpeg;base64,${file.data}`;
+      }
+      setPhotos(photosInStorage);
+    };
+    loadSaved();
+  }, []);
   const takePhoto = async () => {
     const photo = await Camera.getPhoto({
       resultType: CameraResultType.Uri,
@@ -20,14 +57,10 @@ export function usePhotoGallery() {
       quality: 100,
     });
     const fileName = new Date().getTime() + '.jpeg';
-    const newPhotos = [
-      {
-        filepath: fileName,
-        webviewPath: photo.webPath,
-      },
-      ...photos,
-    ];
+    const savedFileImage = await savePicture(photo, fileName);
+    const newPhotos = [savedFileImage, ...photos];
     setPhotos(newPhotos);
+    Storage.set({ key: PHOTO_STORAGE, value: JSON.stringify(newPhotos) });
   };
 
   return {
@@ -39,4 +72,21 @@ export function usePhotoGallery() {
 export interface UserPhoto {
   filepath: string;
   webviewPath?: string;
+}
+
+export async function base64FromPath(path: string): Promise<string> {
+  const response = await fetch(path);
+  const blob = await response.blob();
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onerror = reject;
+    reader.onload = () => {
+      if (typeof reader.result === 'string') {
+        resolve(reader.result);
+      } else {
+        reject('method did not return a string');
+      }
+    };
+    reader.readAsDataURL(blob);
+  });
 }
